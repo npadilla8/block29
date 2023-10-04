bde3cf485314079e45d26f1851c938a852660eb8
Nicole Padilla <npadilla8@bloomberg.net>
2023-10-04 17:57:32 -0400

    Block29 workshop React has been created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div> Puppy Bowl React Project! </div>
+    </>
+  )
+}
+
+export default App
