87ba445d2d2f970cb266af69fa518f3948dedfea
Nicole Padilla <npadilla8@bloomberg.net>
2023-10-05 10:19:06 -0400

    Uploading the Navigatino bar and routes has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import {useState} from 'react';
 import './App.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AllPlayers from './components/AllPlayers';
+import SinglePlayers from './components/SinglePlayers';
+import Nav from "./components/Nav";
+
+const theme = createTheme ({
+  palette: { 
+    mode:"dark"
+  },
+});
+
+// You always want to wrap up your entire App in specified theme provider 
+// In the Theme provider you can pass the theme prop
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div> Puppy Bowl React Project! </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <div> Puppy Bowl React Project
+        <Nav />
+        <Routes>
+          <Route path="/" element={<AllPlayers />} />
+          <Route path="/:id" element={<SinglePlayers />} />
+      </Routes>
+       </div>
+    </ ThemeProvider>
   )
-}
+};
 
 export default App
